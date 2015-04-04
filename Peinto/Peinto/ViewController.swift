73deeb21bb4b30a2c24a1728dd74c0cffa743143
tidalboot <tidalboot@googlemail.com>
2015-04-04 18:19:48 +0100

    Initial Xcode commit

---
+++ Peinto/Peinto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Peinto
+//
+//  Created by Nick Jones on 04/04/2015.
+//  Copyright (c) 2015 Nick Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
