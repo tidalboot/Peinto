f7f8af979335f95d17bc9e88e922fd159a9965c9
tidalboot <tidalboot@googlemail.com>
2015-04-19 22:57:11 +0100

    Added a whole bunch of styling along with app icons

---
+++ Peinto/Peinto/LaunchScreenController.swift
@@ -0,0 +1,23 @@
+//
+//  LaunchScreenController.swift
+//  Peinto
+//
+//  Created by Nick Jones on 19/04/2015.
+//  Copyright (c) 2015 Nick Jones. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenController: UIViewController {
+
+    
+    @IBOutlet var mainView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        mainView.backgroundColor = UIColor(red: 0.941, green: 0.5, blue: 0.5, alpha: 1)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
